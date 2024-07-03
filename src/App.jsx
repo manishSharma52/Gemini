214dd9ba2d89165660f56211f66d401484110450
Your Name <you@example.com>
2024-07-03 15:49:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SideBar from './components/Sidebar/SideBar'
+import Main from './components/Main/Main'
+
+function App() {
+  return (
+    <>
+    <SideBar/>
+    <Main/>
+      
+    </>
+  )
+}
+
+export default App
